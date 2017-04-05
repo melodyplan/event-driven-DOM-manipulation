6b777f58d03f6a85566c5e6a73b5ec46405a9417
Melody Plan <melodyplan@gmail.com>
2017-04-04 20:54:46 -0700

    fizzbuzz

---
+++ fizzbuzz.js
@@ -0,0 +1,11 @@
+console.log("Hello")
+function positiveNumber() {
+  var fizz
+  //is associated with a "<div class="fizz-buzz-item"> and .fizz
+  //replaces integers divisible by 3
+  var buzz 
+  //is associated with class .buzz
+  //replaces integers divisible by 5
+  var fizzbuzz
+  //is associated with class fizzbuzz
+  //replaces integers disible by 5 and 3
