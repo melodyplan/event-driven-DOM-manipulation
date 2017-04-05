752da7e1dfd7fe3ff484b3061fb2f61c5f1d0fdc
Melody Plan <melodyplan@gmail.com>
2017-04-05 07:55:32 -0700

    final try for lightbulb-toggle before i looked at solution

--- lightbulb-toggle.js
+++ lightbulb-toggle.js
@@ -1,19 +1,38 @@
 $(function() {
-  $("js-lightbulb").click(function(bulbOn) {
+  $( "js-lightbulb" ).click(function(bulbOn) {
     //something that gets the lightbulb to turn on
-    var bulbOn = 
+    //var bulbOn = 
     );
   });
   
-$("js-lightbulb").on(
+$( "js-lightbulb" ).on(
   "click change", // Bind handlers for multiple events
   function(bulbOff) {
     //someting that sets all the light bulbs to off
-    ("lightbulb").off("click"); //?
+    //( "lightbulb" ).off( "click" ); //?
 );
+    
+var bulbOn = function() { 
+  ( "js-lightbulb" ).on( "click" ); 
+};
+var bulbOff = function() { 
+  (" js-lightbulb" ).off( "click" ); 
+};
+ 
+$( " js-lightbulb " ).on( "click", /*something about the LB magically turning on*/ );
+$( " js-lightbulb " ).off( "click", /*something about the other LBs staying off*/ ); 
 
-  
-$("lightbulb").on("click", function(event) {
+/*  
+$( "lightbulb" ).on( "click", function(event) {
   this.remove();
   });
 });
+*/
+
+/*i could also use an if and else right? like, ```if``` one
+one light is on all other lights are off? wait, that doesn't
+really work because i don't really have an else to go along.
+if one light is one, all other lights must be off, and that's
+always, and constantly toggled, so no if-else statement.
+
+i just don't know how to make the light go on and off*/   
