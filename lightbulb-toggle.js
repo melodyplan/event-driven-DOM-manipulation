031267bbce443859f89d06bcc58978088113a70e
Melody Plan <melodyplan@gmail.com>
2017-04-04 21:37:34 -0700

    lightbulb-toggle

---
+++ lightbulb-toggle.js
@@ -0,0 +1,12 @@
+$(function() {
+  $("lightbulb").click(function(event) {
+    //something that gets the lightbulb to turn on
+    );
+  });
+  
+  $("lightbulb").on("click", function(event) {
+    //something that gets lightbulb to turn off?
+  });
+});
+
+//referencing event delegation
